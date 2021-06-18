d471362a674e19f9d4be7bf12a3d8be62f1cfd87
Mohaan Balakrishnan <mohaanbalakrishnan@gmail.com>
2021-06-18 18:01:36 +0530

    Memory Leak Played

--- StringMemory/StringExplained.cs
+++ StringMemory/StringExplained.cs
@@ -1,17 +1,31 @@
 namespace play_csharp.StringMemory
 {
     using BenchmarkDotNet.Attributes;
+    using System.Text;
 
     [MemoryDiagnoser]
     public class StringExplained
     {
         [Benchmark]
-        public void DoProcess()
+        public string DoProcess_String()
         {
+            string outText = string.Empty;
             for (int i = 0; i < 10000; i++)
             {
-                var outText = $"This is a sample text{i}";
+                outText += $"This is a sample text{i}";
             }
+            return outText;
+        }
+
+        [Benchmark]
+        public string DoProcess_StringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append($"This is a sample text{i}");
+            }
+            return sb.ToString();
         }
     }
 }
